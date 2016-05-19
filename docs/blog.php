74d005246d4608f0214b0bcb481ee52c1cdf3121
jilliansol <jms865@cornell.edu>
2016-05-18 23:19:09 -0400

    first change

---
+++ docs/blog.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>fatvegan.me</title>
+	<link type="text/css" rel="stylesheet" href="../css/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	<script src="docs/script.js"></script>   
+</head>
+<body class="no-back">
+	<div class="blogs">
+		<div class="blog">
+			<img class="blog-pic" src="../pics/black-woman.jpg">
+			<div class="topic">TOPIC</div>
+			
+			<div class="date-share">
+				<div class="date">Topic | date</div>
+				<div class="share">
+					<img src="../icons/facebook.svg">
+				</div>
+			</div>
+		</div>
+		
+		<div class="blog">
+			<img class="blog-pic" src="../pics/black-woman.jpg">
+			<div class="topic">TOPIC</div>
+			
+			<div class="date-share">
+				<div class="date"> Topic | date</div>
+				<div class="share">
+					<img src="../icons/facebook.svg">
+				</div>
+			</div>
+		</div>
+		
+				<div class="blog">
+			<img class="blog-pic" src="../pics/black-woman.jpg">
+			<div class="topic">TOPIC</div>
+			
+			<div class="date-share">
+				<div class="date"> Topic | date</div>
+				<div class="share">
+					<img src="../icons/facebook.svg">
+				</div>
+			</div>
+		</div>
+				
+		<div class="blog">
+			<img class="blog-pic" src="../pics/black-woman.jpg">
+			<div class="topic">TOPIC</div>
+			
+			<div class="date-share">
+				<div class="date"> Topic | date</div>
+				<div class="share">
+					<img src="../icons/facebook.svg">
+				</div>
+			</div>
+		</div>
+		
+		
+		<div class="blog">
+			<img class="blog-pic" src="../pics/black-woman.jpg">
+			<div class="topic">TOPIC</div>
+			
+			<div class="date-share">
+				<div class="date"> Topic | date</div>
+				<div class="share">
+					<img src="../icons/facebook.svg">
+				</div>
+			</div>
+		</div>
+		
+		<div class="blog">
+			<img class="blog-pic" src="../pics/black-woman.jpg">
+			<div class="topic">TOPIC</div>
+			
+			<div class="date-share">
+				<div class="date"> Topic | date</div>
+				<div class="share">
+					<img src="../icons/facebook.svg">
+				</div>
+			</div>
+		</div>
+	</div><!--.blogs-->
+	
+	
+	
+	
+	<div class="nav">
+		<h1>FATVEGAN.ME</h1>
+		<h5><a href=#>subscribe</a></h5>
+	</div>
+</body>
+    
---
+++ index.html
@@ -0,0 +1,19 @@
+<!DOCTYPE html class="back">
+<head>
+    <meta charset="UTF-8">
+    <title>fatvegan.me</title>
+	<link type="text/css" rel="stylesheet" href="css/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	<script src="docs/script.js"></script>   
+</head>
+<body class="back">
+	<div class="landing-page">
+		<h1>FATVEGAN.ME</h1>
+		<br/>
+		<a href=#>BLOG</a>
+		<div class="credit">
+			credits
+		</div>
+	</div><!--.landing-->
+</body>
+    
